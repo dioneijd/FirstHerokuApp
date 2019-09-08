ad735509e85e463b701e4a9c96eae5a7e3b35048
Dionei <dionei.jd@gmail.com>
2019-09-08 20:48:14 -0300

    Alterado o indexjs

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//express()
+//  .use(express.static(path.join(__dirname, 'public')))
+//  .set('views', path.join(__dirname, 'views'))
+//  .set('view engine', 'ejs')
+//  .get('/', (req, res) => res.render('pages/index'))
+//  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+const app = express()
+
+const route = express.Router()
+
+route.get('/', (req, res) => {return res.send('Hello World')})
+
+
+app.use(route)
+
+app.listen(5000)
 
 
 //import express from 'express';
